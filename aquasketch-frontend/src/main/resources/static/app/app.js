89621c46560da2bbc7dc3b529196c12a03cdea08
Marco Eigletsberger <marco.eigletsberger@gmail.com>
2016-07-11 20:43:19 +0200

    added eraser

--- aquasketch-frontend/src/main/resources/static/app/app.js
+++ aquasketch-frontend/src/main/resources/static/app/app.js
@@ -101,8 +101,6 @@
                     var canvas = document.getElementById('canvas-helper');
                     var ctx = canvas.getContext('2d');
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    console.log(canvas.width);
-                    console.log(canvas.height);
                     img.onload = function () {
                         $scope.$apply(function () {
                             var layer = _.find(ctrl.sketch.layers, {id: ctrl.sketch.activeLayer});
@@ -132,7 +130,6 @@
                 $http.post('/logout', {}).success(function () {
                     $window.location.href = '/login';
                 }).error(function (data) {
-                    console.log("Logout failed")
                 });
             };
         });
@@ -163,9 +160,7 @@
             };
             this.save = function (sketch) {
                 $http.post('/sketches', sketch).then(function (response) {
-                    console.log(response);
                 }, function (response) {
-                    console.log(response);
                 });
                 localStorageService.set(sketch.id, sketch);
             };
@@ -256,6 +251,32 @@
                         return canv;
                     };
 
+                    var deletePointsIn = function (line, x1, y1, x2, y2) {
+                        var newLines = [];
+                        var currentLine = {
+                            points: []
+                        };
+                        newLines.push(currentLine);
+                        _.forEach(line.points, function (point) {
+                            if (point[0] >= x1 && point[1] >= y1
+                                && point[0] <= x2 && point[1] <= y2) {
+                                currentLine = {
+                                    points: []
+                                };
+                                newLines.push(currentLine);
+                            } else {
+                                currentLine.points.push(point);
+                            }
+                        });
+                        return newLines;
+                    };
+
+                    var deletePointsInLayer = function (layer, x1, y1, x2, y2) {
+                        layer.lines = _.flatMap(layer.lines, function (line) {
+                            return deletePointsIn(line, x1, y1, x2, y2);
+                        });
+                    };
+
                     var layer_img_tmp = {};
                     var repaint_layer = function (layer) {
                         var layer_ctx = getCanvas(layer);
@@ -317,9 +338,9 @@
                         main_ctx.lineWidth = 1.3;
                         main_ctx.lineJoin = 'round';
                         main_ctx.lineCap = 'round';
-                        _.forEach(scope.drawing.layers, function (layer) {
-                            repaint_layer(layer);
-                        });
+                        // _.forEach(scope.drawing.layers, function (layer) {
+                        repaint_layer(layer());
+                        // });
                     };
                     scope.$watch('drawing', function (sketch) {
                         var layer_ids = _.map(sketch.layers, 'id');
@@ -339,12 +360,19 @@
                     var mouseDown = false;
                     var startX, startY, lastX, lastY;
                     elem[0].addEventListener("mousemove", _.throttle(function (e) {
-                        if (mouseDown) {
+                        if (scope.drawing.eraser) {
+                            var rect = elem[0].getBoundingClientRect();
+                            var x = e.clientX - rect.left;
+                            var y = e.clientY - rect.top;
+                            scope.$apply(function () {
+                                deletePointsInLayer(layer(), x - 4, y - 4, x + 4, y + 4);
+                            });
+                        } else if (mouseDown) {
                             var rect = elem[0].getBoundingClientRect();
                             var x = e.clientX - rect.left;
                             var y = e.clientY - rect.top;
                             var d = Math.sqrt(Math.pow(lastX - x, 2) + Math.pow(lastY - y, 2));
-                            if (d > 4) {
+                            if (d > 1) {
                                 lastX = x;
                                 lastY = y;
                                 if (e.altKey) {
@@ -358,30 +386,36 @@
                                 });
                             }
                         }
-                    }, 24), false);
+                    }, 10), false);
                     elem[0].addEventListener("mousedown", function (e) {
-                        mouseDown = true;
-                        var rect = elem[0].getBoundingClientRect();
-                        lastX = startX = e.clientX - rect.left;
-                        lastY = startY = e.clientY - rect.top;
-                        scope.$apply(function () {
-                            layer().lines.push({points: [[startX, startY]]});
-                        });
+                        if (scope.drawing.eraser) {
+                        } else {
+                            mouseDown = true;
+                            var rect = elem[0].getBoundingClientRect();
+                            lastX = startX = e.clientX - rect.left;
+                            lastY = startY = e.clientY - rect.top;
+                            scope.$apply(function () {
+                                layer().lines.push({points: [[startX, startY]]});
+                            });
+                        }
                     }, false);
                     elem[0].addEventListener("mouseup", function (e) {
-                        mouseDown = false;
-                        var rect = elem[0].getBoundingClientRect();
-                        var x = e.clientX - rect.left;
-                        var y = e.clientY - rect.top;
-                        if (e.altKey) {
-                            y = startY;
+                        if (scope.drawing.eraser) {
+                        } else {
+                            mouseDown = false;
+                            var rect = elem[0].getBoundingClientRect();
+                            var x = e.clientX - rect.left;
+                            var y = e.clientY - rect.top;
+                            if (e.altKey) {
+                                y = startY;
+                            }
+                            if (e.shiftKey) {
+                                x = startX;
+                            }
+                            scope.$apply(function () {
+                                layer().lines[layer().lines.length - 1].points.push([x, y]);
+                            });
                         }
-                        if (e.shiftKey) {
-                            x = startX;
-                        }
-                        scope.$apply(function () {
-                            layer().lines[layer().lines.length - 1].points.push([x, y]);
-                        });
                     }, false);
                     elem[0].addEventListener("mouseout", function (e) {
 //                            findxy('out', e)
